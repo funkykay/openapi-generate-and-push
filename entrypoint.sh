f5495e2de12232d9ade41cce23f1ff1d54a172f3
Kay Donner <kay.donner@outlook.de>
2021-10-17 23:07:07 +0200

    dqwdqwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
-api_url="https://pokeapi.co/api/v2/pokemon/${INPUT_POKEMON_ID}"
+echo "$INPUT_API_SPEC"
+echo "$INPUT_APISPEC"
+echo "$INPUT_APISPEC"
+
+echo "$INPUT_GENERATOR"
+
+echo "$INPUT_CONFIG_FILE"
+
+api_url="https://pokeapi.co/api/v2/pokemon/1"
 echo $api_url
 
 pokemon_name=$(curl "${api_url}" | jq ".name")
